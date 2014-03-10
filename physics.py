3a7d9303c86f27a08f95460dec34190e3dcb75a0
Nathan Alday <n.c.alday@gmail.com>
2014-03-10 18:47:04 -0500

    Moving to a L1 norm, 2D based physics

--- physics.py
+++ physics.py
@@ -1,7 +1,11 @@
+"""Subspace physics will be 2-D, L1 norm"""
+
 class GlobalConstants:
     """Class containing global constants like the speed of light"""
-    speed_of_light = 3e8
-    base_timestep = 1 #1 second
+    speed_of_light = 3*10**8
+    tick_lengths = 10 #10 second ticks
+    dimensions = 2
+    norm = 1
 
 global global_constants
 global_constants = GlobalConstants()
@@ -11,51 +15,96 @@ class Vector:
     def __init__(self, *values ):
         self.values = tuple(values)
 
-    def __get_item__(self, index ):
+    def __getitem__(self, index ):
         return self.values[index]
 
     def __len__( self ):
         return len( self.values ) 
 
     def scale( self, scalar ):
-        return Vector( [value*scalar for value in self.values] )
+        return Vector( *[value*scalar for value in self.values] )
+
+    def divide( self, scalar ):
+        return Vector( *[value//scalar for value in self.values] )
 
     def add( self, other ):
-        return Vector( [ self[index]+other[index] for index in range(len(self)) ] )
+        return Vector( *[ self[index]+other[index] for index in range(len(self)) ] )
 
     def subtract( self, other ):
-        return Vector( [ self[index]-other[index] for index in range(len(self)) ] )      
+        return Vector( *[ self[index]-other[index] for index in range(len(self)) ] )
 
-def new_vector( length, value = 0.0 ):
+    def __abs__(self):
+        return Vector( *[abs(value) for value in self.values] )
+
+    def norm( self ):
+        """L1 length"""
+        return sum( self )
+
+    def distance( self, other ):
+        return self.subtract(other).norm()
+
+    def __repr__(self):
+        return "Vector({values})".format(values=','.join([str(value) for value in self.values]))
+
+    def __add__(self, other ):
+        return self.add(other)
+
+    def __sub__(self, other ):
+        return self.subtract(other)
+
+def new_vector( length, value = 0 ):
     """Construct a vector of length `length` with the same value in each element"""
-    return Vector( [value]*length )
+    values = [value]*length
+    return Vector( *values )
 
-class PhysicalObject:
-    """The base class of physical objects"""
+def isqrt(nn):
+    """Integer square root ripped shamelessly from StackOverflow:
+http://stackoverflow.com/questions/15390807/integer-square-root-in-python
+and
+http://programmingpraxis.com/2012/06/01/square-roots/"""
+    xx = nn
+    yy = (xx + nn // xx) // 2
+    while yy < xx:
+        xx = yy
+        yy = (xx + nn // xx) // 2
+    return xx
+
+class MassiveObject:
+    """The base class of massive objects"""
     def __init__(self):
-        self.mass = 1.0
-        self.radius = 1.0
-        self.position = new_vector( 3 )
-        self.velocity = new_vector( 3 )
-        self.acceleration = new_vector( 3 )
+        self.mass = 1
+        self.size = 1
+        self.position = new_vector( global_constants.dimensions )
+        self.velocity = new_vector( global_constants.dimensions )
+        self.acceleration = new_vector( global_constants.dimensions )
 
     def set_acceleration( self, force_vector ):
-        self.acceleration = force_vector.scale( 1.0/self.mass )
+        self.acceleration = force_vector.divide( self.mass  )
 
     def current_distance( self, other ):
         """Distance at current moment in time"""
-        pass
+        return (self.distance-other.distance).norm()
 
     def minimum_distance_across_previous_tick( self, other ):
         """Minimum distance between self and other across the previous tick,
         so we check for collisions after advancing"""
-        #determine times at which minimum occurs
+        #determine times at which potential minima occur
+
+        a1 = self.acceleration
+        a2 = other.acceleration
+
+        v1 = self.velocity
+        v2 = other.velocity
+
+
+        #if the relative accelerations are nonzero
+            
+            
         
         #keep negative time solutions since we're looking backwards
-        pass
 
 
-class Module( PhysicalObject ):
+class Module( MassiveObject ):
     """Modules make up ships"""
     def __init__(self):
         self.is_powered = False
@@ -73,8 +122,13 @@ def _test_suite():
     Vector()
     PhysicalObject()
     Module()
-    PhysicalObject()
+    MassiveObject()
     return True
+
+if __name__=="__main__":
+    #Manual test code
+    a=new_vector(2)
+    b=Vector(-1,-2)
 
         
         
